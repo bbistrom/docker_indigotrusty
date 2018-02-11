eee09fcf1cc935ae379de5d12a4fa3f43571087d
benji555552 <benjamin.bistrom@gmail.com>
2018-02-11 16:09:49 +0100

    dockerfile and scripts

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Check args
+if [ "$#" -ne 1 ]; then
+  echo "usage: ./run.sh IMAGE_NAME"
+  return 1
+fi
+
+# Get this script's path
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+set -e
+
+# Run the container with shared X11
+nvidia-docker run\
+  --net=host\
+  --env "DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  -e SHELL\
+  -e DOCKER=1\
+  -v "$HOME:$HOME:rw"\
+  -v "/tmp/.X11-unix:/tmp/.X11-unix:rw"\
+  -it $1 $SHELL
